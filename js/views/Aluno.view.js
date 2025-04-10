bb8d9d2c506d1dcf9f1f598fa1db35893fb15918
MauricioVazz <mauricio.vaz@aluno.ifsp.edu.br>
2025-04-10 16:41:56 -0300

    ajustando edit aluno

--- js/views/Aluno.view.js
+++ js/views/Aluno.view.js
@@ -1,55 +1,52 @@
-class AlunoView {
-    constructor(table, materias) {
-        this.tableList = table
-        this.tableHeader = table.querySelector('thead')
-        this.tableBody = table.querySelector('tbody')
-        this.materias = ["backend_1", "frontend_2", "bancodados", "ferramentas"]
-        this.materias = materias
+class AlunoView{
+	constructor(table, materias) {
+		this.tableList = table
+		this.tableHeader = this.tableList.querySelector('thead')
+		this.tableBody = this.tableList.querySelector('tbody')
+		this.materias = materias
 
-        this.renderHeader()
-    }
+		this.renderHeader()
+	}
 
-    renderHeader() {
-        const htmlHeader = document.createElement('tr')
-        htmlHeader.innerHTML = '<td>Nome</td>'
+	renderHeader() {
+		const htmlHeader = document.createElement('tr')
+		htmlHeader.innerHTML = '<td>Nome</td>'
+		const htmlHeaderMaterias = this.materias.map(materia => {
+			return `<td>${materia.nome}</td>`
+		}).join('')
+		htmlHeader.innerHTML += htmlHeaderMaterias
+		this.tableHeader.appendChild(htmlHeader)
+	}
 
-        const htmlHeaderMaterias = this.materias.map(materia => {
-            return `<td>${materia.nome}</td>`
-        }).join('')
+	render(alunos) {
+		this.tableBody.innerHTML = ''
+		alunos.forEach(aluno => {
+			let htmlRow = document.createElement('tr')
+			htmlRow.innerHTML = `<td>${aluno.nome}</td>`
+			let encontrado = false
+			this.materias.forEach(materia => {
+				if (materia._id in aluno.notas) {
+					encontrado = true
+				}
+			})
 
-        htmlHeader.innerHTML += htmlHeaderMaterias
+			if(encontrado) {
+				this.materias.forEach(materia => {
+					htmlRow.innerHTML += `<td>
+					${aluno.media[materia._id] !== undefined ? aluno.media[materia._id] :
+						`<a href="edit.html?id=${aluno._id}">Incluir Nota</a>`}
+					</td>`
+				})
+			} else {
+				htmlRow.innerHTML += `<td colspan="${this.materias.length}">
+					<a href="edit.html?id=${aluno._id}">
+						Incluir Notas
+					</a>
+				</td>`
+			}
 
-        this.tableHeader.appendChild(htmlHeader)
-    }
+			this.tableBody.appendChild(htmlRow)
+		})
+	}
 
-    render(alunos) {
-        this.tableBody.innerHTML = ''
-        alunos.forEach(aluno => {
-            let htmlrow = document.createElement('tr')
-            htmlrow.innerHTML = `<td>${aluno.nome}</td>`
-            let encontrado = false
-            this.materias.forEach(materia => {
-                if (materia._id in aluno.media) {
-                    encontrado = true
-                }
-            })
-
-            if (encontrado) {
-                this.materias.forEach(materia => {
-                    htmlrow.innerHTML += `<td>
-                    ${aluno.media[materia._id] !== undefined ? aluno.media[materia._id] :
-                        `<a href="edit.html?id=${aluno._id}">Incluir Nota</a>`}
-                        </td>`
-                })
-            } else {
-                htmlrow.innerHTML += `<td colspan="${this.materias.length}">
-                    <a href="edit.html?id=${aluno._id}">
-                        Incluir Notas
-                    </a>
-                </td>`
-            }
-
-            this.tableBody.appendChild(htmlrow)
-        })
-    }
 }
